a5c68033523fb68579e37ca5f0810c805ee5574a
fufu <2034301295@qq.comm>
2023-01-23 00:37:22 +0800

    6 # Please enter the commit message for your changes. Lines starting

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,18 +1,18 @@
-let myButton=document.querySelector('button')
-let myHeading = document.querySelector("h1");
-function setUserName(){
-    let myName=prompt('请输入你的名字');
-    localStorage.setItem('name',myName);
-    if(myName==='fufu'||myName==='holiofox'){
-    alert('yes,约约 love '+myName)}
-    else{alert('NO,约约 does not love '+myName)}
+let myButton = document.querySelector("button");
+function setUserName() {
+  let myName = prompt("请输入你的名字");
+  localStorage.setItem("name", myName);
+  if (myName === "fufu" || myName === "holiofox") {
+    alert("YES,约约 love " + myName);
+  } else {
+    alert("NO,约约 does not love " + myName);
+  }
 }
-if(!localStorage.getItem('name')){
-    setUserName();
-}else{
-    let storedName=localStorage.getItem('name');
-    myHeading.textContent='你好呀，'+storedName;
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  alert("你好，欢迎来到fufu的小窝！");
 }
-myButton.onclick=function(){
-    setUserName();
-}
+myButton.onclick = function () {
+  setUserName();
+};
